4a48625eecddcedc94f110b9a7ee1e1acde94e95
leixyou <leixyou@gmail.com>
2018-02-13 10:23:01 +0800

    xmrig
    xmrig

---
+++ start.cmd
@@ -0,0 +1,3 @@
+@echo off
+D:\xmrig.exe -o pool.aeon.hashvault.pro:5555 --algo=cryptonight-lite -u Wmryc3xTrZnRnZRg19QLzGQvzjzSqy24wWGGnf8WEFd8K4KNhjvqGMHAPTtMh2MfvxFoLwJshorTxL3ZL15UFFBX1jECtneEx -p x --av=4 --donate-level=1
+pause
